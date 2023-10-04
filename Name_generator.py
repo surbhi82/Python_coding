ffdc07e2709c81847e87f47e533be8db5890665d
surbhi82 <63370502+surbhi82@users.noreply.github.com>
2023-10-04 12:37:58 +0530

    Add files via upload

---
+++ Name_generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Welcome to  name Generator")
+#2. Ask the user for the city that they grew up in.
+city=input("enter city ")
+print(city)
+#3. Ask the user for the name of a pet.
+pet=input("enter name of the pet ")
+print(pet)
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name "+city +" " +pet)
+
